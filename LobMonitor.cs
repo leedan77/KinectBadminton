47901cb9c5db672c50ec172fb4c0ca812791beb4
ShaoAn Chien <sac840711@gmail.com>
2016-08-24 02:19:02 +0800

    complete check step forward

--- LobMonitor.cs
+++ LobMonitor.cs
@@ -8,6 +8,18 @@ namespace Microsoft.Samples.Kinect.RecordAndPlaybackBasics
 {
     class LobMonitor
     {
+        public struct Vector3
+        {
+            public double x, y, z, d;
+
+            public Vector3(double vx, double vy, double vz)
+            {
+                x = vx;
+                y = vy;
+                z = vz;
+                d = Math.Sqrt(Math.Pow(vx, 2) + Math.Pow(vy, 2) + Math.Pow(vz, 2));
+            }
+        }
         public struct JointCoord
         {
             public double x, y, z;
@@ -20,6 +32,8 @@ namespace Microsoft.Samples.Kinect.RecordAndPlaybackBasics
         }
         private List<Frames> FrameList;
         private List<int> result;
+        private double spineShoulderBaseDiff = 0;
+        private double initAnkleRightZ = 0;
 
         public LobMonitor(List<Frames> frameList)
         {
@@ -29,14 +43,47 @@ namespace Microsoft.Samples.Kinect.RecordAndPlaybackBasics
 
         public void start()
         {
+            GenerateCompareData();
             int nowFrame = 0;
             nowFrame = CheckWristUp(nowFrame);
-            nowFrame = CheckWristTurn(nowFrame);
+            //nowFrame = CheckWristTurn(nowFrame);
+            nowFrame = CheckStepForward(nowFrame);
         }
 
         public List<int> GetResult()
         {
             return this.result;
+        }
+
+
+        private void GenerateCompareData()
+        {
+            int spineShoulderBaseCount = 0;
+            foreach (Frames Frame in FrameList)
+            {
+                JointCoord spineShoulder = new JointCoord(0, 0, 0);
+                JointCoord spineBase = new JointCoord(0, 0, 0);
+                JointCoord ankleRight = new JointCoord(0, 0, 0);
+                foreach (Joints joint in Frame.jointList)
+                {
+                    if (joint.jointType == "SpineShoulder")
+                        spineShoulder = new JointCoord(joint.x, joint.y, joint.z);
+                    else if (joint.jointType == "SpineBase")
+                        spineBase = new JointCoord(joint.x, joint.y, joint.z);
+                    else if (joint.jointType == "AnkleRight")
+                        ankleRight = new JointCoord(joint.x, joint.y, joint.z);
+                }
+                Vector3 spineShoulderBaseVec = new Vector3(spineShoulder.x - spineBase.x, spineShoulder.y - spineBase.y, spineShoulder.z - spineBase.z);
+                if (spineShoulder.x != 0)
+                {
+                    spineShoulderBaseCount++;
+                    this.spineShoulderBaseDiff += spineShoulderBaseVec.d;
+                }
+                if (ankleRight.z != 0 && this.initAnkleRightZ == 0)
+                    this.initAnkleRightZ = ankleRight.z;
+            }
+            this.spineShoulderBaseDiff /= spineShoulderBaseCount;
+            Console.WriteLine(this.spineShoulderBaseDiff);
         }
 
         private int CheckWristUp(int nowFrame)
@@ -53,32 +100,58 @@ namespace Microsoft.Samples.Kinect.RecordAndPlaybackBasics
                         handRight = new JointCoord(joint.x, joint.y, joint.z);
                 }
                 double thumbHandYDiff = thumbRight.y - handRight.y;
-                //Debug(i, thumbHandYDiff);
                 if (thumbHandYDiff > 0.01)
-                {
-                    Console.WriteLine(i + " Wrist up");
-                    this.result.Add(i);
-                    return i;
-                }
+                    return Record(i, "Wrist up");
             }
             return this.FrameList.Count;
         }
 
         private int CheckWristTurn(int nowFrame)
         {
+            double nowThumbHandXDiff = 0;
+            double prevThumbHandXDiff = 0;
             for (int i = nowFrame; i < this.FrameList.Count; i++)
             {
-                JointCoord thumbRight = new JointCoord(0, 0, 0);
+                JointCoord handTipRight = new JointCoord(0, 0, 0);
                 JointCoord handRight = new JointCoord(0, 0, 0);
                 foreach (Joints joint in this.FrameList[i].jointList)
                 {
-                    if (joint.jointType == "ThumbRight")
-                        thumbRight = new JointCoord(joint.x, joint.y, joint.z);
+                    if (joint.jointType == "HandTipRight")
+                        handTipRight = new JointCoord(joint.x, joint.y, joint.z);
                     else if (joint.jointType == "HandRight")
                         handRight = new JointCoord(joint.x, joint.y, joint.z);
                 }
-                double thumbHandXDiff = thumbRight.x - handRight.x;
-                Debug(i, thumbHandXDiff);
+                nowThumbHandXDiff = handTipRight.x - handRight.x;
+                Debug(i, nowThumbHandXDiff);
+                if(prevThumbHandXDiff < 0 && nowThumbHandXDiff > 0)
+                    return Record(i, "Wrist turn");
+                prevThumbHandXDiff = nowThumbHandXDiff;
+            }
+            return this.FrameList.Count;
+        }
+
+
+
+        private int CheckStepForward(int nowFrame)
+        {
+            for (int i = nowFrame; i < this.FrameList.Count; i++)
+            {
+                JointCoord ankleRight = new JointCoord(0, 0, 0);
+                JointCoord spineShoulder = new JointCoord(0, 0, 0);
+                JointCoord spineBase = new JointCoord(0, 0, 0);
+                foreach (Joints joint in this.FrameList[i].jointList)
+                {
+                    if (joint.jointType == "AnkleRight")
+                        ankleRight = new JointCoord(joint.x, joint.y, joint.z);
+                    else if (joint.jointType == "SpineShoulder")
+                        spineShoulder = new JointCoord(joint.x, joint.y, joint.z);
+                    else if (joint.jointType == "SpineBase")
+                        spineBase = new JointCoord(joint.x, joint.y, joint.z);
+                }
+                double stepSpineRatio = (this.initAnkleRightZ - ankleRight.z) / this.spineShoulderBaseDiff;
+                if (stepSpineRatio > 1.5)
+                    return Record(i, "Step forward");
+                //Debug(i, ankleRight.z);
             }
             return this.FrameList.Count;
         }
@@ -86,6 +159,13 @@ namespace Microsoft.Samples.Kinect.RecordAndPlaybackBasics
         private void Debug(int i, double value)
         {
             Console.WriteLine("Frame: "+ i + ", " + value);
+        }
+
+        private int Record(int i, String criticalPoint)
+        {
+            Console.WriteLine(i + " " + criticalPoint);
+            this.result.Add(i);
+            return i;
         }
     }
 }
